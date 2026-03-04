c91107f64a51d60418e67debd5f71d8aae49c42e
jefaras <jefaraz@gmail.com>
2026-03-04 11:52:31 +0200

    fix: remove accidentally committed dev cache and type files from git
    Remove .next/dev, .next/types, .next/trace-build from tracking.
    Only .next/standalone and .next/static should be in git.

--- .next/dev/static/chunks/app_[lang]_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__d5e8f080._.css",
-  "static/chunks/node_modules_c75b1df6._.js",
-  "static/chunks/_12146f59._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_[lang]_page_tsx_c1c228a0._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/components_18966ed7._.js",
-  "static/chunks/node_modules_8ba37bd5._.js"
-],
-    source: "dynamic"
-});
