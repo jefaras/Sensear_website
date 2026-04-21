ac3e88e10668bc53a8ba99edc17bdbb4105e65fb
jefaras <jefaraz@gmail.com>
2026-04-21 15:42:19 +0300

    Revert "Add GTM SPA pageview tracking"
    This reverts commit f5209d042e63950449ee7703309eb1ba9a382eb8.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from 'next'
 import { headers } from 'next/headers'
-import { Suspense } from 'react'
 
 import '@/app/globals.css'
 
-import { GtmPageview } from '@/components/analytics/GtmPageview'
 import { ClientEnhancements } from '@/components/ClientEnhancements'
 import { OrganizationJsonLd, LocalBusinessJsonLd, WebSiteJsonLd } from '@/components/JsonLd'
 import { Footer } from '@/components/Footer'
@@ -139,9 +137,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                         {children}
                     </main>
                     <Footer lang={lang} dict={dict} />
-                    <Suspense fallback={null}>
-                        <GtmPageview />
-                    </Suspense>
                     <ClientEnhancements />
                 </MotionProvider>
             </body>
